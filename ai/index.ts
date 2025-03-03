83987628a0133a6922e8dc46fd2cc72d7b431ed4
sorvx <sorvx.com@outlook.com>
2025-03-03 16:54:27 +0545

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -2,17 +2,13 @@ import { google } from "@ai-sdk/google";
 import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 
 import { customMiddleware } from "./custom-middleware";
-import {
-  customProvider,
-  extractReasoningMiddleware,
-} from 'ai';
 
 export const geminiProModel = wrapLanguageModel({
-  model: google("gemini-2.0-flash-thinking-exp-01-21"),
-  middleware: extractReasoningMiddleware({ tagName: 'think' }),
+  model: google("gemini-2.0-flash"),
+  middleware: customMiddleware,
 });
 
 export const geminiFlashModel = wrapLanguageModel({
-  model: google("gemini-2.0-flash-thinking-exp-01-21"),
-  middleware: extractReasoningMiddleware({ tagName: 'think' }),
+  model: google("gemini-2.0-flash"),
+  middleware: customMiddleware,
 });
