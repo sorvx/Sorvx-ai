fcdfdbc65b6c561fefd82e192cd8709e3a02b659
sorvx <sorvx.com@outlook.com>
2025-04-04 15:21:32 +0545

    Update route.ts

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,2 @@
-// app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth"
-import { authOptions } from "@/app/(auth)/auth"
 
-const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
